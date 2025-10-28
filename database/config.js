d51d15d47d4d0531ce712395006ce5706a205f61
DilanMamani <160673314+DilanMamani@users.noreply.github.com>
2025-10-28 19:12:21 -0400

    Comment out database connection setup
    Comment out Sequelize initialization code in config.js

--- database/config.js
+++ database/config.js
@@ -37,6 +37,18 @@ const sequelize = process.env.DATABASE_URL
       }
     );
 
+/*const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    dialectModule: pg, 
+    logging: false,
+  }
+);*/
 const dbConnection = async () => {
   try {
     await sequelize.authenticate();
