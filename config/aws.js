a107b5ff02a5e71f66d19a39146f4432a03f82e0
Ivonne Micaela Colque Murillo <colquemurilloivonne@gmail.com>
2026-06-02 15:52:35 -0400

    cambios de opensearch

--- config/aws.js
+++ config/aws.js
@@ -1,23 +1,36 @@
 const { S3Client } = require('@aws-sdk/client-s3');
 const { TextractClient } = require('@aws-sdk/client-textract');
+const { Client } = require('@opensearch-project/opensearch');
+
+const region = process.env.AWS_REGION;
 
 const s3 = new S3Client({
-  region: process.env.AWS_REGION,
+  region,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
 });
 
 const textract = new TextractClient({
-  region: process.env.AWS_REGION,
+  region,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
+});
+
+// OpenSearch (Con Usuario Maestro)
+const opensearch = new Client({
+  node: process.env.OPENSEARCH_ENDPOINT,
+  auth: {
+    username: process.env.OPENSEARCH_USERNAME,
+    password: process.env.OPENSEARCH_PASSWORD
+  }
 });
 
 module.exports = {
   s3,
   textract,
+  opensearch
 };
